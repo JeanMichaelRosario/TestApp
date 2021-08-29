e205b198dc8e41b2cb518a5c4c39832489ba1665
Jean Michael Rosario <jmrosario94@gmail.com>
2021-08-29 11:00:14 -0400

    agregando las acciones para los controladores

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,112 @@
+﻿using TestApp.Db;
+using TestApp.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientesController : Controller
+    {
+        TestDbContext dbContext;
+        ILogger<ClientesController> _logger;
+
+        public ClientesController(TestDbContext context, ILogger<ClientesController> logger)
+        {
+            dbContext = context;
+            _logger = logger;
+        }
+        // GET: ClientesController
+        public ActionResult Index()
+        {
+            return Json(dbContext.Clientes
+                .Include(x => x.Empresa)
+                .Include(x => x.DireccionClientes)
+                .ToList());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Details(int id)
+        {
+            try
+            {
+                var cliente = dbContext.Clientes
+                .Include(x => x.Empresa)
+                .Include(x => x.DireccionClientes)
+                .First(x => x.Id == id);
+
+                return Json(cliente);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+                return BadRequest("Este cliente no existe");
+            }
+        }
+
+        // POST: ClientesController/Create
+        [HttpPost]
+        public ActionResult Create(Cliente cliente)
+        {
+            try
+            {
+                dbContext.Clientes.Add(cliente);
+                dbContext.SaveChanges();
+                return Ok("Cliente creado exitosamente");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+                return BadRequest("No se ha podido crear este cliente, revisa tu datos e intentalo nuevamente");
+            }
+        }
+
+        // POST: ClientesController/Edit/5
+        [HttpPut("{id}")]
+        public ActionResult Edit(int id, Cliente clienteActualizado)
+        {
+            try
+            {
+                var cliente = dbContext.Clientes.Include(x => x.DireccionClientes).First(x => x.Id == id);
+                cliente.Nombre = clienteActualizado.Nombre;
+                foreach (var item in cliente.DireccionClientes)
+                {
+                    dbContext.Remove(item);
+                }
+                cliente.Direcciones = clienteActualizado.Direcciones;
+                dbContext.SaveChanges();
+                return Json(cliente);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+                return BadRequest("No se ha podido acutalizar este cliente, revisa tu datos e intentalo nuevamente");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var cliente = dbContext.Clientes.Include(x => x.DireccionClientes).First(x => x.Id == id);
+                dbContext.RemoveRange(cliente.DireccionClientes);
+                dbContext.Remove(cliente);
+                dbContext.SaveChanges();
+                return Ok("El cliente ha sido eliminado exitosamente");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+                return BadRequest("El cliente no se ha podido eliminar, favor intentar nuevamente");
+            }
+        }
+    }
+}
---
+++ Controllers/EmpresasController.cs
@@ -0,0 +1,113 @@
+﻿using TestApp.Db;
+using TestApp.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmpresasController : Controller
+    {
+        TestDbContext dbContext;
+        ILogger<EmpresasController> _logger;
+
+        public EmpresasController(TestDbContext context, ILogger<EmpresasController> logger)
+        {
+            dbContext = context;
+            _logger = logger;
+        }
+        [HttpGet]
+        public IEnumerable<Empresa> Index()
+        {
+            return dbContext.Empresas
+                .Include(x => x.Clientes)
+                .ThenInclude(x =>x.DireccionClientes)
+                .ToArray();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Details(int id)
+        {
+            try
+            {
+                var empresa = dbContext.Empresas
+                    .Include(x => x.Clientes)
+                    .ThenInclude(x => x.DireccionClientes)
+                    .First(x => x.Id == id);
+
+                return Json(empresa);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+                return BadRequest("Esta empresa no existe");
+            }
+        }
+
+        // POST: ClientesController/Create
+        [HttpPost]
+        public ActionResult Create(Empresa empresa)
+        {
+            try
+            {
+                dbContext.Empresas.Add(empresa);
+                dbContext.SaveChanges();
+                return Json(empresa);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+                return BadRequest("No se ha podido crear esta empresa, revisa tu datos e intentalo nuevamente");
+            }
+        }
+
+        // POST: ClientesController/Edit/5
+        [HttpPut("{id}")]
+        public ActionResult Edit(int id, Empresa edirEmpresa)
+        {
+            try
+            {
+                var empresa = dbContext.Empresas.Include(x => x.Clientes)
+                    .ThenInclude(x => x.DireccionClientes).First(x => x.Id == id);
+                empresa.Nombre = edirEmpresa.Nombre;
+                dbContext.SaveChanges();
+                return Json(empresa);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+                return BadRequest("No se ha podido acutalizar esta empresa, revisa tu datos e intentalo nuevamente");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var empresa = dbContext.Empresas.Include(x => x.Clientes)
+                    .ThenInclude(x => x.DireccionClientes).First(x => x.Id == id);
+                foreach (var cliente in empresa.Clientes)
+                {
+                    dbContext.RemoveRange(cliente.DireccionClientes);
+                }
+                dbContext.RemoveRange(empresa.Clientes);
+                dbContext.Remove(empresa);
+                dbContext.SaveChanges();
+                return Ok("La empresa ha sido eliminada exitosamente");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+                return BadRequest("La empresa no se ha podido eliminar, favor intentar nuevamente");
+            }
+        }
+    }
+}
